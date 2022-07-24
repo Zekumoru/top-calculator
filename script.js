72f28b62ff50dd68d31c41b29a1b2b252a21f871
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-24 22:27:17 +0200

    Lower cased operation function names

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function Add(a, b) {
+function add(a, b) {
   return a + b;
 }
 
-function Subtract(a, b) {
+function subtract(a, b) {
   return a - b;
 }
 
-function Multiply(a, b) {
+function multiply(a, b) {
   return a * b;
 }
 
-function Divide(a, b) {
+function divide(a, b) {
   return a / b;
 }
