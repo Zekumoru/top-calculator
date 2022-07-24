339acf6bf2201debd5bc24bb4b891485f075c394
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-25 00:32:54 +0200

    Finish buttons functionalities

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 
 const output = document.querySelector('.output');
 
-let firstInput = 0;
+let firstInput;
 let secondInput;
+let enteredSecondInput = false;
+let operator = null;
 
 document.querySelector('.clear').addEventListener('click', (e) => {
   output.textContent = '0';
   firstInput = 0;
+  operator = null;
 });
 
 document.querySelectorAll('.numbers button').forEach((button) => {
   if (Number.isNaN(+button.textContent)) return;
   
   button.addEventListener('click', (e) => {
-    if (output.textContent === '0') {
+    if (!enteredSecondInput && operator !== null) {
       output.textContent = button.textContent;
+      secondInput = +output.textContent;
+      enteredSecondInput = true;
       return;
     }
 
-    output.textContent += button.textContent;
-    secondInput = +output.textContent;
+    (output.textContent === '0')? output.textContent = button.textContent : output.textContent += button.textContent;
+    (enteredSecondInput)? secondInput = +output.textContent : firstInput = +output.textContent;
   });
 });
 
 document.querySelectorAll('.operators button').forEach((button) => {
   button.addEventListener('click', (e) => {
-    console.log(button.textContent);
+    if (operator !== null && (enteredSecondInput || button.textContent === '=')) {
+      firstInput = operate(operator, firstInput, secondInput);
+      output.textContent = firstInput;
+      enteredSecondInput = false;
+    }
+    
+    if (button.textContent === '=') return;
+
+    operator = button.textContent;
+    secondInput = firstInput;
   });
 });
 
