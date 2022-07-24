955f4ca69c271c9a926a9162134366066af7d3ea
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-25 00:47:12 +0200

    Create updateDisplay function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let enteredSecondInput = false;
 let operator = null;
 
 document.querySelector('.clear').addEventListener('click', (e) => {
-  display.textContent = '0';
+  updateDisplay(0);
   firstInput = 0;
   operator = null;
 });
@@ -17,13 +17,13 @@ document.querySelectorAll('.numbers button').forEach((button) => {
   
   button.addEventListener('click', (e) => {
     if (!enteredSecondInput && operator !== null) {
-      display.textContent = button.textContent;
-      secondInput = +display.textContent;
+      updateDisplay(button.textContent);
+      secondInput = +button.textContent;
       enteredSecondInput = true;
       return;
     }
 
-    (display.textContent === '0')? display.textContent = button.textContent : display.textContent += button.textContent;
+    (display.textContent === '0')? updateDisplay(button.textContent) : updateDisplay(display.textContent + button.textContent);
     (enteredSecondInput)? secondInput = +display.textContent : firstInput = +display.textContent;
   });
 });
@@ -32,7 +32,7 @@ document.querySelectorAll('.operators button').forEach((button) => {
   button.addEventListener('click', (e) => {
     if (operator !== null && (enteredSecondInput || button.textContent === '=')) {
       firstInput = operate(operator, firstInput, secondInput);
-      display.textContent = firstInput;
+      updateDisplay(firstInput);
       enteredSecondInput = false;
     }
 
@@ -42,6 +42,10 @@ document.querySelectorAll('.operators button').forEach((button) => {
     secondInput = firstInput;
   });
 });
+
+function updateDisplay(string) {
+  display.textContent = (typeof string === 'number')? +string.toFixed(10) : string;
+}
 
 function add(a, b) {
   return a + b;
