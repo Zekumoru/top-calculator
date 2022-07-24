436c063b42b759d9d27fab04c2867c616472232f
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-24 23:33:06 +0200

    Add event listeners on buttons

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+
+const output = document.querySelector('.output');
+
+let firstInput = 0;
+let secondInput;
+
+document.querySelector('.clear').addEventListener('click', (e) => {
+  output.textContent = '0';
+  firstInput = 0;
+});
+
+document.querySelectorAll('.numbers button').forEach((button) => {
+  if (Number.isNaN(+button.textContent)) return;
+  
+  button.addEventListener('click', (e) => {
+    if (output.textContent === '0') {
+      output.textContent = button.textContent;
+      return;
+    }
+
+    output.textContent += button.textContent;
+    secondInput = +output.textContent;
+  });
+});
+
+document.querySelectorAll('.operators button').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(button.textContent);
+  });
+});
+
 function add(a, b) {
   return a + b;
 }
