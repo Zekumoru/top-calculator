8b0318aa0639ac2c715a2415080c719a5e3254b1
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-25 18:51:43 +0200

    Move arithmetic functions to class Calculator

--- script.js
+++ script.js
@@ -117,41 +117,41 @@ function Calculator() {
     };
   }
 
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function operate(op, a, b) {
+    switch(op) {
+      case '+':
+        return add(a, b);
+      case '-':
+        return subtract(a, b);
+      case '*':
+        return multiply(a, b);
+      case '/':
+        return divide(a, b);
+      default:
+        return null;
+    }
+  }
+
   function resolveActive() {
     if (evaluated) {
       active = firstInput;
       evaluated = false;
     }
-  }
-}
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function operate(op, a, b) {
-  switch(op) {
-    case '+':
-      return add(a, b);
-    case '-':
-      return subtract(a, b);
-    case '*':
-      return multiply(a, b);
-    case '/':
-      return divide(a, b);
-    default:
-      return null;
   }
 }
